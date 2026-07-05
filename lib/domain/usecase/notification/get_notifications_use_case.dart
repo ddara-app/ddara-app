6fb2576402a95264da0134042c6a0db544d2829f
kimdoyun98 <rladlsxo1250@gmail.com>
2026-07-06 01:03:09 +0900

    chore: test 로그 코드 삭제

--- lib/domain/usecase/notification/get_notifications_use_case.dart
+++ lib/domain/usecase/notification/get_notifications_use_case.dart
@@ -11,16 +11,9 @@ class GetNotificationsUseCase {
     NotificationCategory category = NotificationCategory.all,
     int size = 20,
   }) async {
-    final test =  await _notificationRepository.getNotifications(
+    return await _notificationRepository.getNotifications(
       category: category,
       size: size,
     );
-
-    print('===================== items(${test.items.length}) =====================');
-    for (var i = 0; i < test.items.length; i++) {
-      print('[$i] ${test.items[i]}');
-    }
-    print('====================================================');
-    return test;
   }
 }
