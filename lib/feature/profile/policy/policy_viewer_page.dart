9d53381db133ca11be4ab974bf3368da2babf84c
kimdoyun98 <rladlsxo1250@gmail.com>
2026-07-21 20:18:02 +0900

    chore: flutter_markdown → flutter_markdown_plus 교체 (discontinued 대응)

--- lib/feature/profile/policy/policy_viewer_page.dart
+++ lib/feature/profile/policy/policy_viewer_page.dart
@@ -4,7 +4,7 @@ import 'package:ddara/core/design_system/design_system.dart';
 import 'package:ddara/l10n/app_localizations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:go_router/go_router.dart';
 
 /// [PolicyViewerPage] 에 넘기는 인자. (제목 + 마크다운 에셋 경로)
