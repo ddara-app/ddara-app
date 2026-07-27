b9f44dec8f59ce96843c55c479c453466fd26b61
kimdoyun98 <rladlsxo1250@gmail.com>
2026-07-27 11:02:32 +0900

    style: 정책 뷰어 본문 좌우 여백을 s5(16)로 통일

--- lib/core/widget/policy/policy_viewer_page.dart
+++ lib/core/widget/policy/policy_viewer_page.dart
@@ -53,8 +53,8 @@ class PolicyViewerPage extends StatelessWidget {
               selectable: true,
               padding: EdgeInsets.only(
                 top: AppSpacing.s3,
-                left: AppSpacing.s4,
-                right: AppSpacing.s4,
+                left: AppSpacing.s5,
+                right: AppSpacing.s5,
                 // 하단 Safe Area 까지 스크롤 영역을 잇되, 마지막 줄이 홈
                 // 인디케이터와 겹치지 않도록 인셋만큼 더 띄운다.
                 bottom: AppSpacing.s6 + MediaQuery.of(context).padding.bottom,
